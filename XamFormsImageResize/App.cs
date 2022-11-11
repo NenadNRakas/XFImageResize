6f4b0a85c764a565dc4215125ed77d4f8b458269
NenadRakas <nenadrakas@outlook.com>
2022-11-11 22:00:07 +0100

    Add project files.

---
+++ XamFormsImageResize/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace XamFormsImageResize
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new HomePage();
+        }
+    }
+}
